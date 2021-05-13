0227103353cb89aae0239a99cdb9e9bfde51cf18
Jacob Toftgaard Rasmussen <1jatora@gmail.com>
2021-05-13 07:21:17 +0200

    finished printArray2

---
+++ Lektion1/printArray2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void createArray(long* a){
+    for(int i = 0; i < 20; i++){
+        a[i] = 10 + i;
+    }
+}
+
+int main(){
+    long a[20];
+    createArray(a);
+    long* x = &a[3];
+    printf("%p", &x);
+
+    return 0;
+}
